a58b34c652f0f97df78c6b8cf2203db3a57e46e0
sekonic <sekonic@gmail.com>
2023-12-05 13:54:34 -0300

    Fetch and Filter logs

--- index.js
+++ index.js
@@ -22,6 +22,46 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
+// Filter log array
+function filterLogArray(arr, from, to, limit) {
+  let results;
+
+  if (!from && !to && !limit) {
+    results = arr;
+  }
+
+  if (from && !to) {
+    let fromDate = new Date(from).getTime();
+    results = arr.filter((d) => {
+      let date = new Date(d.date).getTime();
+      if (date >= fromDate) return d;
+    });
+  }
+
+  if (!from && to) {
+    let toDate = new Date(to).getTime();
+    results = arr.filter((d) => {
+      let date = new Date(d.date).getTime();
+      if (date <= toDate) return d;
+    });
+  }
+
+  if (from && to) {
+    let toDate = new Date(to).getTime();
+    let fromDate = new Date(from).getTime();
+    results = arr.filter((d) => {
+      let date = new Date(d.date).getTime();
+      if (date >= fromDate && date <= toDate) return d;
+    });
+  }
+
+  if (limit) {
+    if (results.length > Number(limit)) results.length = Number(limit);
+  }
+
+  return results;
+}
+
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -31,15 +71,19 @@ app.get('/', (req, res) => {
 app.post('/api/users/', async (req, res) => {
   let { username } = await req.body;
   let userData;
-  let data = await User.find({ username: username });
+  try {
+    let data = await User.find({ username: username });
 
-  data.length === 0
-    ? (userData = await User.create({ username: username }))
-    : (userData = data[0]);
+    data.length === 0
+      ? (userData = await User.create({ username: username }))
+      : (userData = data[0]);
 
-  res
-    .status(200)
-    .send(JSON.stringify({ username: userData.username, _id: userData._id }));
+    res
+      .status(200)
+      .send(JSON.stringify({ username: userData.username, _id: userData._id }));
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Add Exercises
@@ -66,7 +110,7 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
       } else {
         let log = data[0].log;
         log.push(newExercise);
-        await User.updateOne({ _id: id }, { $set: { "log": log } });
+        await User.updateOne({ _id: id }, { $set: { log: log } });
         newExercise._id = data[0]._id;
         newExercise.username = data[0].username;
         res.status(200).send(JSON.stringify(newExercise));
@@ -83,6 +127,42 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
 app.get('/api/users/', async (req, res) => {
   let data = await User.find().select('username _id');
   res.status(200).send(JSON.stringify(data));
+});
+
+// Logs
+app.get('/api/users/:_id/logs', async (req, res) => {
+  let { _id } = req.params;
+  let { from, to, limit } = req.query;
+
+  if (mongoose.isValidObjectId(_id)) {
+    try {
+      let data = await User.findOne({ _id });
+      if (!data) {
+        res.json({ error: '_ID NOT FOUND' });
+      } else {
+        let arr = data.log;
+
+        let logs = filterLogArray(arr, from, to, limit);
+
+        let count = logs.length;
+
+        let response = {
+          _id: data._id,
+          username: data.username,
+        };
+        if (from) response.from = from;
+        if (to) response.to = to;
+        response.count = count;
+        response.log = logs;
+
+        res.status(200).json(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.json({ error: 'NOT VALID _ID' });
+  }
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
