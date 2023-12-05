1f361fe71734ecbf1be58cf2e7dc3963f4c8c22c
sekonic <sekonic@gmail.com>
2023-12-04 22:48:18 -0300

    Add exercises to DB

--- index.js
+++ index.js
@@ -45,21 +45,44 @@ app.post('/api/users/', async (req, res) => {
 // Add Exercises
 app.post('/api/users/:_id/exercises', async (req, res) => {
   let { description, duration, date } = await req.body;
-  let id = await req.body[":_id"];
-  console.log( { id } );
-  console.log( { description } );
-  console.log( { duration } );
-  console.log( { date } );
+  let id = await req.body[':_id'];
 
-  res.status(200).send("INFO RECIBIDA");
+  // Date handling
+  date
+    ? (date = new Date(date).toDateString())
+    : (date = new Date().toDateString());
+
+  let newExercise = {
+    description,
+    duration,
+    date,
+  };
+
+  if (mongoose.isValidObjectId(id)) {
+    try {
+      let data = await User.find({ _id: id });
+      if (data.length === 0) {
+        res.send('_ID NOT FOUND');
+      } else {
+        let log = data[0].log;
+        log.push(newExercise);
+        await User.updateOne({ _id: id }, { $set: { "log": log } });
+        newExercise._id = data[0]._id;
+        newExercise.username = data[0].username;
+        res.status(200).send(JSON.stringify(newExercise));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(404).send('NOT VALID _ID');
+  }
 });
 
 // List all users
 app.get('/api/users/', async (req, res) => {
   let data = await User.find().select('username _id');
-  res
-    .status(200)
-    .send(JSON.stringify(data));
+  res.status(200).send(JSON.stringify(data));
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
